b944d6891b439b205f65fd621fc37b31cb4e60c1
abhishak26 <abhishaktimothy@gmail.com>
2026-07-07 21:35:11 +0530

    fix backend vercel routing

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,12 @@ const workoutRoutes = require('./routes/workouts')
 const app = express()
 
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
 })
-app.use(cors())
-app.use(express.json())
 
 //routes
 app.use('/api/workouts', workoutRoutes)
@@ -31,4 +30,3 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
-
